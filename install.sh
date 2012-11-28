614ed1e266a70f92bbf8dbb6f150b3212743d6be
yubo <yubo@yubo.org>
2012-11-28 15:41:16 -0800

    delete bundle

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#git clone git@github.com:yubo/home.git ~/.yubo
+
+
+install_file()
+{
+	src=`pwd`"/"$1
+	des="$HOME/$1"
+	echo "
+	src $src
+	des $des"
+	if [ -f $des -o -d $des ]; then
+		mv -f $des $des".backup"
+	fi
+	ln -s $src $des
+}
+
+install_file bin
+install_file .vimrc
+install_file .vim
+install_file .bash_profile
+install_file .bashrc
+install_file .dircolors
+
+git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+echo "
+vim
+BundleInstall
+"
+
+
+
+
+
+
