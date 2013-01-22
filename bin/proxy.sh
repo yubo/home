a9bcbe63076dd7aaaab94ebdcc901ad7fcb36ab3
yubo <yubo@yubo.org>
2013-01-22 01:07:30 -0800

    update proxy

--- bin/proxy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export HTTP_PROXY="http://192.168.122.1:3128"
-export http_proxy=$HTTP_PROXY
-
-export HTTPS_PROXY="https://192.168.122.1:3128"
-export https_proxy=$HTTPS_PROXY
-
-export GIT_SSH="${HOME}/bin/socks-ssh.sh"
-export GIT_PROXY_COMMAND="${HOME}/bin/socks-gw.sh"
--- bin/socks-ssh.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ssh -o ProxyCommand="${HOME}/bin/socks-gw.sh %h %p" $@
