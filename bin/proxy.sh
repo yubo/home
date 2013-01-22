ecb155149c43ee850ce24f9f26f49b7d21cc1cd5
yubo <yubo@yubo.org>
2013-01-21 23:57:48 -0800

    add http proxy

--- bin/proxy.sh
+++ bin/proxy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export HTTP_PROXY=192.168.122.1:3128
+export HTTP_PROXY="http://192.168.122.1:3128"
 export http_proxy=$HTTP_PROXY
 
-export HTTPS_PROXY=192.168.122.1:3128
+export HTTPS_PROXY="https://192.168.122.1:3128"
 export https_proxy=$HTTPS_PROXY
 
 export GIT_SSH="${HOME}/bin/socks-ssh.sh"
