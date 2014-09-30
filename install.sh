3f24b1a2210794cdeeaf847ca5ebb9bd3d704f95
yubo <yubo@xiaomi.com>
2014-09-30 22:49:05 +0800

    update

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ install_file .bashrc
 install_file .dircolors
 install_file .gitconfig
 install_file .fonts
+install_file .tmux.conf
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
